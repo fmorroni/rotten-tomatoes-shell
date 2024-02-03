23a600217c4559f167a6f0017b38ae94140e2b6a
Franco Morroni <fmorroni99@gmail.com>
2024-02-03 01:30:32 -0300

    feat: Better command arguments

--- src/main.ts
+++ src/main.ts
@@ -21,25 +21,49 @@ const reqBody = {
 };
 
 interface MediaInfo {
-  type: string;
-  title: string;
-  description: string;
-  releaseYear: string;
-  genres: Array<string>;
-  runTime: {
+  type?: string;
+  title?: string;
+  description?: string;
+  releaseYear?: string;
+  genres?: Array<string>;
+  runTime?: {
     hours: number;
     mins: number;
   };
-  rating: {
+  rating?: {
     audienceScore: number;
     criticsScore: number;
     tmScore: number;
   };
-  cast: Array<string>;
+  cast?: Array<string>;
 }
 
-export async function getMediaInfo(mediaTitle: string): Promise<MediaInfo> {
+export async function getMediaInfo(
+  mediaTitle: string,
+  limit: number,
+  year?: number,
+): Promise<MediaInfo[] | null> {
   reqBody.requests[0].query = mediaTitle;
+  const res = await (
+    await fetch(apiUrl, {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+    })
+  ).json();
+
+  if (!res?.results?.[0].hits) return null;
+
+  let resHits: Array<any> = res?.results?.[0].hits
+  if (year)  resHits = resHits.filter(hit => hit.releaseYear === year)
+
+  const hits: MediaInfo[] = [];
+  for (let i = 0; i < resHits.length && i < limit; ++i) {
+    hits.push(parseHit(resHits[i]));
+  }
+  return hits;
+}
+
+function parseHit(hit: any): MediaInfo {
   const {
     type,
     title,
@@ -49,16 +73,18 @@ export async function getMediaInfo(mediaTitle: string): Promise<MediaInfo> {
     runTime,
     rottenTomatoes,
     castCrew,
-  } = (
-    await (
-      await fetch(apiUrl, {
-        method: "POST",
-        body: JSON.stringify(reqBody),
-      })
-    ).json()
-  ).results[0].hits[0];
+  } = hit;
+
   const hours = Math.floor(runTime / 60);
   const mins = runTime - hours * 60;
+  const rating = rottenTomatoes
+    ? {
+        audienceScore: rottenTomatoes.audienceScore,
+        criticsScore: rottenTomatoes.criticsScore,
+        tmScore: rottenTomatoes.newAdjustedTMScore,
+      }
+    : undefined;
+
   return {
     type,
     title,
@@ -66,24 +92,7 @@ export async function getMediaInfo(mediaTitle: string): Promise<MediaInfo> {
     releaseYear,
     genres,
     runTime: { hours, mins },
-    rating: {
-      audienceScore: rottenTomatoes.audienceScore,
-      criticsScore: rottenTomatoes.criticsScore,
-      tmScore: rottenTomatoes.newAdjustedTMScore,
-    },
+    rating,
     cast: castCrew.cast,
   };
 }
-
-// const url = 'https://www.rottentomatoes.com/m/before_i_fall'
-
-// function getInfo(mediaTitle)
-// fetch(url).then(res=>res.text()).then(res=>{
-//   const {mediaType, modal: {audienceScoreAll, tomatometerScoreAll}} = JSON.parse(res.match(/<script id="scoreDetails" .*?>(\{.*\})<\/script/)[1]);
-//   return {
-//     mediaType,
-//     audienceScore: audienceScoreAll,
-//     criticsScore: tomatometerScoreAll,
-//   }
-// }
-// )
--- src/rotten-tomatoes.ts
+++ src/rotten-tomatoes.ts
@@ -1,4 +1,4 @@
-import { Command } from "commander";
+import { Command } from "@commander-js/extra-typings";
 const program = new Command();
 
 import { getMediaInfo } from "./main.js";
@@ -6,8 +6,20 @@ import { getMediaInfo } from "./main.js";
 program
   .description("Get media info from rotten tomatoes")
   .version("1.0.0")
-  .argument("<mediaTitle>", "Name of the movie of series to search for.")
-  .action(async (mediaTitle: string) => {
-    console.log(await getMediaInfo(mediaTitle));
+  .argument("<mediaTitle...>", "Name of the movie of series to search for.")
+  .option(
+    "-l, --limit <limit>",
+    "Get the top <limit> results from the search",
+    "1",
+  )
+  .option(
+    "-y, --year <releaseYear>",
+    "Only get results from media released in <releaseYear>",
+  )
+  .action(async (mediaTitle, opts) => {
+    const year = opts.year ? parseInt(opts.year) : undefined;
+    console.log(
+      await getMediaInfo(mediaTitle.join(" "), parseInt(opts.limit), year),
+    );
   })
   .parseAsync();
